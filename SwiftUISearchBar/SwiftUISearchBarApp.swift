04a134f513384a76353e9f92d1cb6e4cf6136350
Zheng <aheze@getfind.app>
2021-09-20 11:51:33 -0700

    Initial Commit

---
+++ SwiftUISearchBar/SwiftUISearchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISearchBarApp.swift
+//  SwiftUISearchBar
+//
+//  Created by Zheng on 9/20/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISearchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
